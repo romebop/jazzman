8588936ccc686afd4fd845780fa9d39bb49b396e
Saerom Park <park.srome@gmail.com>
2025-01-14 21:55:06 -0600

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://romebop.github.io',
-  base: '/jazzman',
   integrations: [react()],
 });
